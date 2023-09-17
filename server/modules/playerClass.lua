002317f70de81d1fd0dec420964add6ce3a09be5
Mycroft <kaseyfitton@hotmail.co.uk>
2023-09-17 22:38:28 +0100

    tweak(server/modules/playerClass): commenting

--- server/modules/playerClass.lua
+++ server/modules/playerClass.lua
@@ -2,6 +2,18 @@
 -- Player Class
 vCore.Players = {}
 
+---@class ExtendedPlayer
+---@field identifier string
+---@field source number
+---@field group string
+---@field name string
+---@field coords table
+---@field metadata table
+---@field Set function
+---@field Get function
+---@field HasGroup function
+---@field TriggerEvent function
+
 ExtendedPlayer = {
     __index = ExtendedPlayer
 }
@@ -16,23 +28,43 @@ function vCore:CreatePlayer(identifer, data)
     xPlayer.coords = { x = 222.2027, y = -864.0162, z = 30.2922, heading = 1.0 }
     xPlayer.metadata = data.metadata
 
-    function xPlayer:set(key, value)
+    function xPlayer:Set(key, value)
         self[key] = value
     end
 
-    function xPlayer:get(key)
+    function xPlayer:Get(key)
         return self[key]
     end
 
-    function xPlayer:hasGroup(group)
+    function xPlayer:HasGroup(group)
         return self.group == group
     end
 
-    function xPlayer:triggerEvent(event, ...)
+    function xPlayer:TriggerEvent(event, ...)
         TriggerClientEvent(event, self.source, ...)
     end
     
     return xPlayer
+end
+
+---@param player number
+---@return ExtendedPlayer
+function vCore:GetPlayerFromId(player)
+    if type(player) ~= "number" then
+        player = tonumber(player)
+    end
+    return self.Players[player]
+end
+
+---@param identifier string
+---@return ExtendedPlayer|nil
+function vCore:GetPlayerFromIdentifier(identifier)
+    for _, player in pairs(self.Players) do
+        if player.identifier == identifier then
+            return player
+        end
+    end
+    return nil
 end
 
 AddEventHandler('playerDropped', function(reason)
