590f0df8f3cf606d356f06097a17a87c8343c94e
Mycroft <kaseyfitton@hotmail.co.uk>
2023-09-17 14:08:02 +0100

    refactor(shared/modules): split config into Server and Client

--- shared/modules/config-handler.lua
+++ shared/modules/config-handler.lua
@@ -1,33 +1,50 @@
-vCore.ConfigStorage = {
-    client = {},
-    server = {}
-}
+if vCore.context == "server" then
+    -- do all server-sided functions here.
+    -- It is Massively important to have the server side handle all the data.
+    vCore.ConfigStorage = KVP("vCore:ConfigStorage")
 
-function vCore:GetConfigOption(name)
-    return vCore.ConfigStorage[self.context][name]
-end
-
-function vCore:GetConfigValue(name)
-    return self:GetConfigOption(name).value
-end
-
-function vCore:AddConfigOption(name, label, defaultData, onChange)
-    local existingData = self:GetConfigOption(name)
-    vCore.ConfigStorage[self.context][name] = {
-        label = label,
-        defaultData = defaultData,
-        value = existingData.value or defaultData,
-        onChange = onChange
-    }
-    if onChange then
-        onChange(existingData.value or defaultData)
+    if not vCore.ConfigStorage then
+        vCore.ConfigStorage = {}
     end
-end
 
-function vCore:SetConfigValue(name, value)
-    local configOption = self:GetConfigOption(name)
-    vCore.ConfigStorage[self.context][name].value = value
-    if configOption.onChange then
-        configOption.onChange(value)
+    function vCore:GetConfigOption(name)
+        return vCore.ConfigStorage[name]
     end
+
+    function vCore:GetConfigValue(name)
+        return self:GetConfigOption(name).value
+    end
+
+    function vCore:AddConfigOption(name, context, label, defaultData, onChange)
+        local existingData = self:GetConfigOption(name)
+        vCore.ConfigStorage[name] = {
+            label = label,
+            defaultData = defaultData,
+            value = existingData.value or defaultData,
+        }
+    end
+
+    function vCore:SetConfigValue(name, value)
+        local existingData = self:GetConfigOption(name)
+        vCore.ConfigStorage[name].value = value
+        TriggerEvent("vCore:ConfigChanged", name, value)
+        TriggerClientEvent("vCore:ConfigChanged", -1, name, value)
+    end
+
+    lib.callback.register('vCore:getConfigValues', function()
+        return vCore.ConfigStorage
+    end)
+else
+    vCore.ConfigStorage = lib.callback.await("vCore:getConfigValues", false)
+    function vCore:GetConfigOption(name)
+        return self.ConfigStorage[name]
+    end
+
+    function vCore:GetConfigValue(name)
+        return self:GetConfigOption(name)?.value
+    end
+
+    vCore:RegisterNetEvent("vCore:ConfigChanged", function(name, value)
+        vCore.ConfigStorage[name].value = value
+    end)
 end
