0e7e70028baf31655c8c3b5f0b883f49caab870e
Mycroft <kaseyfitton@hotmail.co.uk>
2023-09-17 13:26:07 +0100

    feat(client/main): basic config options

--- client/main.lua
+++ client/main.lua
@@ -2,14 +2,25 @@ local baseConfig = {
     {
         name = "npc_population",
         label = "Enable NPC Population",
-        default = GetConvarInt("vcore:npc_population", 1) == 1,
+        default = GetConvarInt("vCore:npc_population", 1) == 1,
     },
     {
         name = "npc_licenseplate",
         label = "Defualt Licencse Plate",
-        default = GetConvar("vcore:npc_licenseplate", ".... ..."),
+        default = GetConvar("vCore:npc_licenseplate", ".... ..."),
         onChange = function(value)
             SetDefaultVehicleNumberPlateTextPattern(-1, value)
         end
     }
 }
+
+for i=1, #baseConfig do
+    local config = baseConfig[i]
+    vCore:AddConfigOption(config.name, config.label, config.default, config.onChange)
+end
+
+AddEventHandler('populationPedCreating', function()
+    if not vCore:GetConfigValue('npc_population') then
+        CancelEvent()
+    end
+end)
