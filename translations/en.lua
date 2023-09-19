d3c6bd9e47594bbb00ca52f5091d3fa7d70a41be
Mycroft <kaseyfitton@hotmail.co.uk>
2023-09-19 01:43:51 +0100

    feat: Create translation file

---
+++ translations/en.lua
@@ -0,0 +1,5 @@
+Translations["en"] = {
+    ["config_title"] = "Server Configuration",
+    ["config_changed"] = "Option has successfully been changed.",
+    ["config_not_changed"] = "Unable to Change Configuration option",
+}
