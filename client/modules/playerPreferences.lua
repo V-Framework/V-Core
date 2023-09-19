21d99a3b9d1d353a79be805e9afd8e2b41cae936
Mycroft <kaseyfitton@hotmail.co.uk>
2023-09-19 01:59:34 +0100

    feat(client/modules/playerPreferences): ox lib menu

--- client/modules/playerPreferences.lua
+++ client/modules/playerPreferences.lua
@@ -11,3 +11,32 @@ end
 function vCore:GetPlayerPreference(key)
     return self.Preferences[key]
 end
+
+function vCore:PreferencesMenu()
+    local elements = {}
+
+    for key, value in pairs(self.Preferences) do
+        elements[#elements + 1] = {
+            label = self:Translate(key),
+            description = self:Translate("current_value", value),
+            key = key,
+        }
+    end
+    
+    lib.registerMenu({
+        id = 'vCore.PreferencesMenu',
+        title = self:Translate('preferences_title'),
+        position = 'top-right',
+        canClose = true,
+        options = elements
+    }, function(selected)
+        local preference = elements[selected].key
+        local input = lib.inputDialog(self:Translate("preference_set_title"), {self:Translate("preference_set", preference)})
+        if not input then
+            return
+        end
+        self:SetPlayerPreference(preference, input[1])
+        lib.showMenu('vCore.PreferencesMenu')
+    end)
+    lib.showMenu('vCore.PreferencesMenu')
+end
--- translations/en.lua
+++ translations/en.lua
@@ -1,5 +1,12 @@
 Translations["en"] = {
+    -- Config Options
     ["config_title"] = "Server Configuration",
     ["config_changed"] = "Option has successfully been changed.",
     ["config_not_changed"] = "Unable to Change Configuration option",
+
+    -- Preferences Menu
+    ["preferences_title"] = "Player Preferences",
+    ["current_value"] = "Current Value: %s",
+    ["preference_set_title"] = "Set Preference",
+    ["preference_set"] = "Enter Preference for %s",
 }
