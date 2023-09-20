08e10bdd805c6570dd5f89d1465a92a70b82c858
Mycroft <kaseyfitton@hotmail.co.uk>
2023-09-20 18:03:37 +0100

    feat(translations): spanish translation
    Ran through DeepL, so may not be too accurate

---
+++ translations/es.lua
@@ -0,0 +1,12 @@
+Translations["es"] = {
+    -- Config Options
+    ["config_title"] = "Configuración del servidor",
+    ["config_changed"] = "La opción se ha cambiado correctamente",
+    ["config_not_changed"] = "No se ha podido cambiar la opción Configuración",
+
+    -- Menú Preferencias
+    ["preferences_title"] = "Preferencias del jugador",
+    ["current_value"] = "Valor actual: %s",
+    ["preference_set_title"] = "Establecer preferencia",
+    ["preference_set"] = "Introducir preferencia para %s",
+}
